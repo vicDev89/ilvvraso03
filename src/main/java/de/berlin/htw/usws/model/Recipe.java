8c1393a13415a0ecc5650d9e657cda32c3d6de06
vicdev89 <victor.del.campo@student.htw-berlin.de>
2018-10-24 19:41:08 +0200

    Recipe Klasse

--- src/main/java/de/berlin/htw/usws/model/Recipe.java
+++ src/main/java/de/berlin/htw/usws/model/Recipe.java
@@ -3,7 +3,10 @@ package de.berlin.htw.usws.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 
 @Getter
@@ -11,5 +14,29 @@ import javax.persistence.Entity;
 @Entity
 public class Recipe {
 
+    private String title;
 
+    @Column
+    private String preparation;
+
+    @OneToMany
+    @Column
+    private List<Ingredient> ingredients;
+
+    @Column
+    private int cookingTime;
+
+    @Column
+    private int preparationTime;
+
+    @OneToMany
+    @Column
+    private List<Opinion> opinions;
+
+    @Column
+    private DifficultyLevel difficultyLevel;
+
+    // Saved as URL
+    @Column
+    private String picture;
 }
