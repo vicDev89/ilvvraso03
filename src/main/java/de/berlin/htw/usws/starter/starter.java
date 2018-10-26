bce5864bbde1f8f91b750fef1fd16fdc77a50949
vicdev89 <victor.del.campo@student.htw-berlin.de>
2018-10-26 08:06:04 +0200

    starter.java weg

--- src/main/java/de/berlin/htw/usws/starter/starter.java
+++ src/main/java/de/berlin/htw/usws/starter/starter.java
@@ -1,13 +1,49 @@
 package de.berlin.htw.usws.starter;
 
 import de.berlin.htw.usws.model.Product;
+import de.berlin.htw.usws.model.Supermarket;
 import de.berlin.htw.usws.webCrawlers.EdekaCrawler;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
-public class starter {
+public class Starter {
 
-    public void main(String[] args) {
+    private static final EntityManagerFactory entityManagerFactory;
+    private static final String PERSISTENCE_UNIT_NAME = "ingrEatDB_unit";
+
+    static {
+
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+
+    }
+
+    public static EntityManager getEntityManager() {
+
+        return entityManagerFactory.createEntityManager();
+
+    }
+
+    public static void main(String[] args) {
+
+
         List<Product> edekaProducts = EdekaCrawler.getAllProducts();
+
+		EntityManager entityManager = getEntityManager();
+		entityManager.getTransaction().begin();
+
+        Product testProduct = new Product("Apfel", Supermarket.EDEKA, 1.50, 2.50);
+        edekaProducts.add(testProduct);
+        for(Product product : edekaProducts) {
+            entityManager.persist(product);
+        }
+
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+
     }
 }
